d60e4dc017f6e5ea64a2cfd7c597d732bebc90a9
amymariewall <amymariewall@gmail.com>
2016-01-10 20:15:37 +0000

    No Multiplier Exercise In Progress

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,2 +1,11 @@
-def multiply(num1, num2)
+puts "Please enter a whole number"
+    first_number=gets.chomp
+    puts "Now enter another whole number"
+    second_number=gets.chomp
+    number_2=second_number.to_i
+    number_1=first_number.to_i
+    product = 0
+    number_2.times do
+    product = product.+(number_1)
 end
+puts product
