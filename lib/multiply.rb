a4111b78504d76b1d03f12ca96a6a1347b72ab93
amymariewall <amymariewall@gmail.com>
2016-01-12 21:44:50 +0000

    Multiplier Response Updated

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,11 +1,11 @@
-def multiplier_method (first_number, second_number)
+def multiply (first_number, second_number)
     number_2=second_number.to_i
     number_1=first_number.to_i
     product = 0
     number_2.times do 
-    product = product.+(number_1)
+        product = product.+(number_1)
 end
     return product
 end
 
-puts multiplier_method "10","15"
+puts multiply "10","15"
