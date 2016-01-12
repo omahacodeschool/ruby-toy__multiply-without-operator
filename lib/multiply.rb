897c126313f626110aeffa95ec0a11b4f01d2dda
Patrick Clark <patclark1958@gmail.com>
2016-01-12 00:48:17 +0000

    not quite it

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,2 +1,22 @@
-def multiply(num1, num2)
+#def multiply(num1, num2)
+#MULTIPLY WITHOUT USING THE TIMES OPERATOR
+#MULTIPLICATION IS JUST ADDITION REPEATED. I NEED
+#TO MAKE A PROGRAM WHERE YOU CAN INPUT TWO WHOLE
+#NUMBERS SO THAT THE FIRST ONE IS ADDED TO ITSELF
+#AND THE SECOND NUMBER IS THE NUMBER OF TIMES THE
+#FIRST ONE IS ADDED TO ITSELF. LIKE 3,5 IS
+#WORKED AS 3+3+3+3+3.
+
+#puts 'Please enter the first number to be multiplied and push the enter key.'
+#first=gets.chomp
+#x=first.to_i
+#puts "Please enter the second number to be multiplied and push the enter key."
+#second=gets.chomp
+#y=second.to_i
+x=5
+y=3
+z=0
+x.times do (y)
+z+=y+y
 end
+puts [z]
