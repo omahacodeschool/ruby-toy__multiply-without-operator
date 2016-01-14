2d57c119cb41ca0987c6e582040270f3b2ddfbc4
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-14 03:22:44 +0000

    fin

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,10 +1,7 @@
 def multiply(num1, num2)
-#ok technically this works... it just isn't very pretty. Im not sure how to only display the last iteration.
-puts "provide variable a"
-a=gets.chomp.to_i
-puts "provide variable b"
-b=gets.chomp.to_i
-c=0
-for loop in 1..b do
-   puts c+=a
+x=0
+for loop in 1..num2
+x+=num1
+end
+x
 end
