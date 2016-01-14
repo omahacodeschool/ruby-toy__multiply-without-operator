55a1bf0598040933ef139586b8ea7b27e686e248
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-14 17:31:29 +0000

    done

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,8 +1,7 @@
 def multiply(num1, num2)
-new_a = []
-(num1).times do |i|  
-    new_a<< (num2)
+    new_a = []
+    (num1).times do |i|  
+      new_a << (num2)
 end
-return new_a.inject{|sum,x| sum + x }
-
+    return new_a.inject { |sum,x| sum + x }
 end
