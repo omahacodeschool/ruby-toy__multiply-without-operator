ebe3381b43d778f9ee9c3b23e4b136c5ea27a3f5
Jillian <jwaggoner1016@gmail.com>
2016-01-15 01:55:11 +0000

    Multiply

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,2 +1,12 @@
-def multiply(num1, num2)
+a = 6
+b = 3
+this_array = Array.new(b) {a}
+sum = 0
+this_array.each do |x|
+  sum += x
 end
+puts sum
+
+I put the solution together piecemeal (by brushing up on blocks and searching for "how to add elements in an array"), so my understanding of how the sum part is working is alright but hazy. I couldn't have slapped that together off the top of my head is what I'm saying. Am I missing out on the value of these exercises if that is the case? I'm trying to get the concepts ingrained as I go along, but I'm finding that to be the hardest and most time-consuming part. Everything I'm seeing looks familiar to me, but the comprehension didn't stick in my head after the first exposure.
+
+Will submit revised code soon taking into account the def/end requirement. Still trying to get my head around that, too.
