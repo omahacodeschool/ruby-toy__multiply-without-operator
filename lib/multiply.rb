9b899afd68962c7c2a7ac40270b2b6d8cedaf6a6
Rachel Lyons <rlyons21@hotmail.com>
2016-06-08 03:59:13 +0000

    Submitting work for assignment

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,2 +1,10 @@
 def multiply(num1, num2)
+    # Initialize the total to be zero
+    total = 0
+    
+    # Add num1 to total num2-times
+    for i in (1..num2) do
+	    total += num1
+    end
+    return total
 end
