670392abe30a6e4ecadb5a6e490d0d14ed48de7e
Alex <noto.alex.j@gmail.com>
2016-06-10 04:33:48 +0000

    Added thought process comments

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -5,3 +5,5 @@ def multiply(num1, num2)
 	end
 	return product
 end
+#Since multiplication is simply repeated addition, I wrote a loop that took a preestablished value, 'product', and added
+# a number, 'num1', to it 'num2' number of times.
