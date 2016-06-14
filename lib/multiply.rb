89a2dcc90736a027262f2578f909f6a92f10fe42
Michael <michaeljducey@gmail.com>
2016-06-14 02:22:08 +0000

    Subitting real attempt at work for multiply-without-operator

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,2 +1,12 @@
 def multiply(num1, num2)
+    total = Array.new
+    if num1 == 0 or num2 == 0
+        return 0
+    else num1.times do 
+        total.push(num2)
+    end
+    sum = 0
+        total.each {|a| sum+=a }
+            return sum
+    end
 end
